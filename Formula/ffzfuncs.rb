0510d2c2d59ae22bf612a099702240055c48feba
Andrey Astashov <mvc.aaa@gmail.com>
2025-07-01 22:15:57 +0300

    checksum added

--- Formula/ffzfuncs.rb
+++ Formula/ffzfuncs.rb
@@ -2,7 +2,7 @@ class Ffzfuncs < Formula
   desc "Zsh functions for ffmpeg re-encoding with tagging"
   homepage "https://github.com/mvcaaa/ffzfuncs"
   url "https://github.com/mvcaaa/ffzfuncs/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "PUT_YOUR_SHA256_HERE"
+  sha256 "4860de6fa77602a9f00a9b7374d4dd61b7840b279d75aca8f9a33b56d6770027"
   version "0.1.0"
 
   def install
